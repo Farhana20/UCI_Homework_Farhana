8a8fd8004e885462a5f5acf1ac8e4b0ad7ce1b09
unknown <shampa00@yahoo.com>
2020-12-13 00:50:30 -0800

    homework update

--- javascript-challenge/static/js/app.js
+++ javascript-challenge/static/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
 
+
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+//console.log(tableData);
+tableData.forEach(function(UFOSightings) {
+
+    console.log(UFOSightings);
+    var row = tbody.append("tr");
+
+    Object.entries(UFOSightings).forEach(function([key, value]){
+        console.log(key, value);
+
+        var cell = row.append("td");
+        cell.text(value);
+
+    });
+    
+     
+
+});
+    
+    
